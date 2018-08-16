14ae51e7a26b2bf6192d9c9b471e0f24a7efdf0c
Grant Kim <grantkim93@gmail.com>
2018-08-15 23:30:10 -0500

    complete redesign

---
+++ js/cards.js
@@ -0,0 +1,65 @@
+/*
+web:
+    integrated language
+    streamlines
+    sound escape
+
+mobile:
+    fruit saver
+
+data:
+    data plus
+    how to kickoff kickstarter
+
+web and data:
+    math visualization
+    machine learning interface
+*/
+
+
+cards = [
+    // grant kim
+    {
+        img_url: '../img/grant-pic.jpg',
+        type: 'personal',
+        title: 'grant kim',
+        summary: `a kid who is way too into minimalistic design. also likes web dev,
+            machine learning, psychology, and rom-coms.`
+    },
+    // integrated language
+    {
+        img_url: '../img/integrated-language.PNG',
+        type: ['web'],
+        title: 'integrated language',
+        summary: 'chrome extension that integrates foreign language into everyday internet usage.'
+    },
+    // streamlines
+    {
+        img_url: '../img/streamlines.PNG',
+        type: ['web'],
+        title: 'streamlines',
+        summary: 'text editor that helps writers keep moving forward.'
+    },
+    // math visualization
+    {
+        img_url: '../img/math-visualization.PNG',
+        type: ['data', 'web'],
+        title: 'math department dashboard',
+        summary: 'dashboard for visualizing trends in duke\'s math department.'
+    },
+    // data plus
+    {
+        img_url: '../img/data-plus.PNG',
+        type: ['data'],
+        title: 'data+',
+        summary: 'analyzing how patient-doctor interaction affects medical decision-making.'
+    },
+    // how to kickoff kickstarter
+    {
+        img_url: '../img/kickstarter.PNG',
+        type: ['data'],
+        title: 'how to kickoff kickstarter',
+        summary: 'uncovering what makes for a successful kickstarter campaign.'
+    },
+    //
+]
--- js/my-js.js
+++ js/my-js.js
@@ -1,7 +1,43 @@
-setTimeout(
-    function() {
-        $('.heading-primary').remove();
-        $('#blocker').remove();
-    }, 3500
-);
+// setTimeout(
+//     function() {
+//         let blocker = document.querySelector('#blocker');
+//         blocker.parentNode.removeChild(blocker);
+//     }, 3000
+// );
+
+// appends boxes to screen
+let c = document.createDocumentFragment();
+cards.forEach(function(card) {
+    let e = document.createElement('div');
+    e.className = 'box';
+
+    // adds front and back to box element
+    let front = e.appendChild(document.createElement('div'));
+    front.className = 'box__side box__side--front';
+    front.style.backgroundImage = `url(${card.img_url})`;
+    front.style.backgroundSize = 'cover';
+
+    let back = e.appendChild(document.createElement('div'));
+    back.className = 'box__side box__side--back';
+
+    // title of project
+    let backTitle = back.appendChild(document.createElement('p'));
+    backTitle.className = 'box__side--back--title';
+    backTitle.textContent = card.title;
+
+    // brief summary of project
+    let backSummary = back.appendChild(document.createElement('p'));
+    backSummary.className = 'box__side--back--summary';
+    backSummary.innerHTML = card.summary;
+    if(card.type != 'personal') {
+        backMoreInfo = back.appendChild(document.createElement('p'));
+        backMoreInfo.className = 'box__side--back--more-info';
+        backMoreInfo.innerHTML = `<a href="#">Learn More</a> - <a href="#">View Project</a>`;
+    }
+    
+    
+    c.appendChild(e);
+})
+document.querySelector('.box-holder').appendChild(c);
+
 
