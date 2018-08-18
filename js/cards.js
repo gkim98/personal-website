37ee9b0dabcaa2b1ab41dbfc9dc214b0f5374672
Grant Kim <grantkim93@gmail.com>
2018-08-17 23:56:11 -0500

    card section finished

--- js/cards.js
+++ js/cards.js
@@ -15,7 +15,8 @@ web and data:
     math visualization
     machine learning interface
 */
-
+integratedLanguageLink = '<a href="https://chrome.google.com/webstore/detail/integrated-language/gecndgpfcacjllppgiffgfdhpdgibjan" target="_blank" class="card__link">Integrated Language</a>'
+dataPlusLink = '<a href="https://bigdata.duke.edu/projects/complex-decisions-real-numbers-medical-decision-making" target="_blank" class="card__link">Data+</a>'
 
 cards = [
     // integrated language
@@ -23,34 +24,65 @@ cards = [
         img_url: '../img/integrated-language.PNG',
         type: ['web'],
         title: 'integrated language',
-        summary: `chrome extension that integrates foreign language into everyday internet usage.`
+        info: 'hackduke (oct 2017), cont. jan 2018',
+        background: "Vocabulary can be the most boring part of learning a language, especially if your tool of choice is endless flashcards.",
+        summary: `${integratedLanguageLink} is a chrome extension that incorporates foreign language into your everyday internet usage, allowing you to learn a language naturally and consistently.`,
+        github: 'https://github.com/shavavo/IntegratedLanguage',
+        tools: ['js']
+    },
+    // sound escape
+    {
+        img_url: '../img/sound-escape.png',
+        type: ['web'],
+        title: 'sound escape',
+        info: 'packhacks (april 2018)',
+        background: "3D audio-recording microphones can cost hundrends, even thousands, of dollars.",
+        summary: "Sound Escape allows you to create 3D soundscapes cheaply and conveniently using any 2D audio clips. Simply position your sounds around the listener to create rich audio environments.",
+        github: "https://github.com/gkim98/sound-escape",
+        tools: ['js', 'react', 'howler.js']
     },
     // streamlines
     {
         img_url: '../img/streamlines.PNG',
         type: ['web'],
         title: 'streamlines',
-        summary: 'text editor that helps writers keep moving forward.'
-    },
-    // math visualization
-    {
-        img_url: '../img/math-visualization.PNG',
-        type: ['data', 'web'],
-        title: 'math department dashboard',
-        summary: 'dashboard for visualizing trends in duke\'s math department.'
+        info: 'feb 2018',
+        background: "One of the biggest sources of writer's block is doubt. It makes writers scrutinize every word they write.",
+        summary: "Streamlines is a text editor that forces writers to move forward and stay in the present by literally making it impossible to look back.",
+        github: "https://github.com/gkim98/streamlines",
+        tools: ['js', 'react', 'firebase']
     },
     // data plus
     {
         img_url: '../img/data-plus.PNG',
         type: ['data'],
-        title: 'data+',
-        summary: 'analyzing how patient-doctor interaction affects medical decision-making.'
+        title: 'complex decisions, real numbers',
+        info: 'may 2018 - aug 2018',
+        background: `With the support of Duke's ${dataPlusLink} program, I spent ten weeks working with fellow students to explore how physician-patient interaction affects medical decision-making.`,
+        summary: 'Using classification models and topic modeling, we were able to extract which parts of conversation were associated with differences in treatment decision.',
+        github: 'https://github.com/sjgarland8/DukeDataPlus-Summer18-Proj17',
+        tools: ['numpy stack', 'scikit-learn', 'nltk', 'gensim', 'xgboost']
+    },
+    // math visualization
+    {
+        img_url: '../img/math-visualization.PNG',
+        type: ['data'],
+        title: 'math department dashboard',
+        info: 'feb 2018 - may 2018',
+        background: "Working with Duke's mathematics department, I created a visualization dashboard to better understand the trends concerning the composition of students taking courses in the department.",
+        summary: '',
+        github: 'https://github.com/gkim98/math-visualization',
+        tools: ['js', 'react', 'victory.js']
     },
     // how to kickoff kickstarter
     {
         img_url: '../img/kickstarter.PNG',
         type: ['data'],
         title: 'how to kickoff kickstarter',
-        summary: 'uncovering what makes for a successful kickstarter campaign.'
+        info: 'jan 2018',
+        background: "During Kickstarter campaigns, you set a fundraising goal, and if you reach it, you get the money. Otherwise, you get nothing. As a result, campaigns have a strategic component.",
+        summary: "In my analysis, I explore the factors that contribute to success on Kickstarter for users to apply to their own campaigns.",
+        github: 'https://github.com/gkim98/kickoff-kickstarter',
+        tools: ['numpy stack', 'seaborn']
     }
 ]
--- js/my-js.js
+++ js/my-js.js
@@ -1,43 +1,125 @@
-// setTimeout(
-//     function() {
-//         let blocker = document.querySelector('#blocker');
-//         blocker.parentNode.removeChild(blocker);
-//     }, 3000
-// );
+// card creation for project type
+let createCards = function(type) {
+    let c = document.createDocumentFragment();
+    cards.forEach(function(card) {
+        // skips projects for non-selected categories
+        if(!card.type.includes(type)) { return; };
 
-// appends boxes to screen
-let c = document.createDocumentFragment();
-cards.forEach(function(card) {
-    let e = document.createElement('div');
-    e.className = 'box';
+        let e = document.createElement('div');
+        e.className = 'card';
 
-    // adds front and back to box element
-    let front = e.appendChild(document.createElement('div'));
-    front.className = 'box__side box__side--front';
-    front.style.backgroundImage = `url(${card.img_url})`;
-    front.style.backgroundSize = 'cover';
+        let content = e.appendChild(document.createElement('div'));
+        content.className = 'card__content';
 
-    let back = e.appendChild(document.createElement('div'));
-    back.className = 'box__side box__side--back';
+        //adds title and info to the card
+        let title = content.appendChild(document.createElement('h1'));
+        title.className = 'card__title';
+        title.textContent = card.title;
+        let info = content.appendChild(document.createElement('h2'));
+        info.className = 'card__info';
+        info.textContent = card.info;
 
-    // title of project
-    let backTitle = back.appendChild(document.createElement('p'));
-    backTitle.className = 'box__side--back--title';
-    backTitle.textContent = card.title;
+        // adds image to the card
+        let img = content.appendChild(document.createElement('div'));
+        img.className = 'card__img';
+        img.style.backgroundImage = `url(${card.img_url})`;
+        
+        // adds text to the cards
+        let text = content.appendChild(document.createElement('div'));
+        text.className = 'card__text';
+        let background = text.appendChild(document.createElement('p'));
+        background.className = 'card__background';
+        background.innerHTML = card.background;
+        let summary = text.appendChild(document.createElement('p'));
+        summary.className = 'card__summary';
+        summary.innerHTML = card.summary;
 
-    // brief summary of project
-    let backSummary = back.appendChild(document.createElement('p'));
-    backSummary.className = 'box__side--back--summary';
-    backSummary.innerHTML = card.summary;
-    if(card.type != 'personal') {
-        backMoreInfo = back.appendChild(document.createElement('p'));
-        backMoreInfo.className = 'box__side--back--more-info';
-        backMoreInfo.innerHTML = `<a href="#">Learn More</a> - <a href="#">View Project</a>`;
+        // adds list of tools 
+        let toolsString = card.tools.join(', ');
+        let toolBox = content.appendChild(document.createElement('p'));
+        toolBox.className = 'card__tool-box';
+        let toolSpan = toolBox.appendChild(document.createElement('span'));
+        toolSpan.textContent = 'tools: ';
+        toolSpan.className = 'card__tool-span';
+        let toolList = toolBox.appendChild(document.createElement('span'));
+        toolList.textContent = toolsString;
+        toolList.className = 'card__tool-list';
+
+        // adds github link
+        let github = content.appendChild(document.createElement('a'));
+        github.href = card.github;
+        github.target = '_blank';
+        let gitLogo = github.appendChild(document.createElement('img'));
+        gitLogo.className = 'card__github';
+        gitLogo.src = '../img/github-logo.png';
+    
+        // adds cards to temporary fragment
+        c.appendChild(e);
+    });
+
+    // returns the fragment 
+    return c;
+}
+
+// removes selected class on click --> change selected
+let removeSelectedClass = function() {
+    let filterItems = document.getElementsByClassName('filter__option');
+    for(let i = 0; i < filterItems.length; i++) {
+        filterItems[i].classList.remove('selected');
     }
-    
-    
-    c.appendChild(e);
-})
-document.querySelector('.box-holder').appendChild(c);
+}
+
+let updateFilter = function(t) {
+    removeSelectedClass();
+    t.classList.add('selected');
+}
+
+let disappearCards = function() {
+    let currentCards = document.getElementsByClassName('card');
+    let cardHolder = document.querySelector('.card-holder');
+    cardHolder.style.opacity = '0';
+}
+
+let addCards = function(cards) {
+    let cardHolder = document.querySelector('.card-holder');
+    cardHolder.innerHTML = "";
+    cardHolder.appendChild(cards);
+    cardHolder.style.opacity = '1';
+}
+
+let replaceCards = function(t) {
+    let selectedOption = t.textContent;
+    let newCards = createCards(selectedOption);
+    disappearCards();
+    setTimeout(function() {
+        addCards(newCards);
+    }, 300);
+}
+
+// when item clicked --> replaces cards with new cards
+let cardItemSelected = function(option) {
+    let t = option.target;
+    if(!t.classList.contains('selected')) {
+        updateFilter(t);
+        replaceCards(t);
+    }
+};
+
+// adds on click listener to the filter options
+let addFilterListeners = function() {
+    let filterOptions = document.getElementsByClassName('filter__option');
+    for(let i = 0; i < filterOptions.length; i++) {
+        filterOptions[i].addEventListener('click', cardItemSelected, false);
+    }
+}
 
 
+let main = function() {
+    // loads up initial cards
+    let firstCards = createCards('web');
+    addCards(firstCards);
+    addFilterListeners();
+}
+
+// runs the javascript for the page
+main();
